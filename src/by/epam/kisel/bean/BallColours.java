e5c5fd56464aa779d0fd4dc09ffabdcb92f037f1
Kate1999 <kate.kisel99@yandex.by>
2021-06-02 21:42:28 +0300

    02.06.21

--- src/by/epam/kisel/bean/BallColours.java
+++
@@ -1,5 +0,0 @@
-package by.epam.kisel.bean;
-
-public enum BallColours {
-	WHITE, BLACK, YELLOW, GREEN, PINK, RED, BLUE, PURPLE, ORANGE
-}
--- src/by/epam/kisel/bean/BasketParams.java
+++
@@ -1,7 +0,0 @@
-package by.epam.kisel.bean;
-
-public interface BasketParams {
-	
-	public double countVolume();
-	public boolean isSmallerThan(Ball ball);
-}
